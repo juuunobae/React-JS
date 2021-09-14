65bdd3393c9f434f2e8ed815ddc475aea465232d
juuunobae <juuunobae@gmail.com>
2021-09-14 12:23:17 +0900

    React JS

---
+++ src/Components/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import Header from "Components/Header";
+import TV from "Routes/TV";
+
+export default () => (
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
+);
